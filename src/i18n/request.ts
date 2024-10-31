0f8111c03b91c51547b82f941aa42b9f36ec8700
seifmegahed <seifmegahed@me.com>
2024-10-31 11:31:28 +0300

    changed folder name to dict

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -13,6 +13,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
   return {
     locale,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    messages: (await import(`messages/${locale}.json`)).default,
+    messages: (await import(`dictionary/${locale}.json`)).default,
   };
 });
